a9bbfecfcc77fd0f90f68d43ecedbb977111d3fc
langelsega02 <langelsega02@gmail.com>
2025-04-10 04:24:37 +0000

    started project financial data fetcher

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 3334
+
+Works are divided by folders, attendance verification assignments are in the attendance folder. Homework assignments are in the assignments folder. And the classlectures folder is for personal note taking from the lectures.
---
+++ assignments/financialDataFetcher/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+
+trait Pricing {
+    fn fetch_price(){
+
+    }
+    fn save_to_file(){
+        
+    }
+}
+    
+struct Bitcoin {
+
+}
+
+impl Pricing for Bitcoin {
+
+}
+
+struct Ethereum {
+
+}
+
+impl Pricing for Ethereum{
+
+}
+
+struct SP500 {
+
+}
+
+impl Pricing for SP500 {
+
+}
+
+    println!("Hello, world!");
+}
